278697ffa55c840eb6a24c6b3fcb4af7dd02a1b6
angel <angelipr@metropolia.fi>
2024-08-20 17:16:21 +0300

    Tehtävä2 ratkaisu

---
+++ Moduuli 2/Tehtävä2.3.py
@@ -0,0 +1,9 @@
+
+kanta_str = input("Suorakulmion kanta: ")
+korkeus_str = input("Suorakulmion korkeus: ")
+kanta = float(kanta_str)
+korkeus = float(korkeus_str)
+pintaala = (kanta*korkeus)
+print("pintaala 3" + str(pintaala))
+piiri = (2*kanta + korkeus*2)
+print("piiri " + str(piiri))
---
+++ Moduuli 2/Tehtävä2.4.py
@@ -0,0 +1,9 @@
+luku1_str = float(input("anna numero: "))
+luku2_str = float(input("anna numero: "))
+luku3_str = float(input("anna numero: "))
+summa = (luku1_str+luku2_str+luku3_str)
+kerto = luku1_str*luku2_str*luku3_str
+keskiarvo = (luku1_str+luku2_str+luku3_str)/3
+print(f"Arvo: {summa}")
+print(f"Arvo: {kerto}")
+print(f"Keskiarvo: {keskiarvo}")
