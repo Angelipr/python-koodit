e1b7453ba662be4bdcff05099eaa43c4710d328c
angel <angelipr@metropolia.fi>
2024-08-27 14:18:14 +0300

    Tehtävä3 ratkaisu

--- Moduuli 3/Tehtävä3.4.py
+++ Moduuli 3/Tehtävä3.4.py
@@ -1,6 +1,8 @@
-vuosiluku = float(input('Anna vuosiluku: '))
-kv = (vuosiluku//4)
-if vuosiluku/4 == kv:
-    print('Vuosi on karkausvuosi ')
-else:
-    print('Vuosi ei ole karkausvuosi')
+
+while True:
+    vuosiluku = float(input('Anna vuosiluku: '))
+    kv = (vuosiluku//4)
+    if vuosiluku%4 == 0 and (vuosiluku%100 != 0 or vuosiluku%400 == 0):
+        print('Vuosi on karkausvuosi')
+    else:
+        print('Vuosi ei ole karkausvuosi')
