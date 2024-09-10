4d8fe60fda426fa85b5f23ad78e454a6bf954a03
angel <angelipr@metropolia.fi>
2024-09-10 14:49:05 +0300

    Tehtävä7 ratkaisu

---
+++ Moduuli 7/Tehtävä7.1.py
@@ -0,0 +1,4 @@
+anna = int(input('Anna kuukauden numero: '))
+lista = ("talvi", "talvi", "kevät", "kevät", "kevät", "kesä", "kesä", "kesä", "syksy", "syksy", "syksy", "talvi")
+kuukaudet = lista[anna-1]
+print(f"{kuukaudet}")
